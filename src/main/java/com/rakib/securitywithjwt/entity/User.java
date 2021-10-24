302dce043eb1f82043983c68b5a999bc82b3d49f
rakibccj <rakibccj@gmail.com>
2021-10-24 21:45:33 +0600

    Set Mapping with User and Role

--- src/main/java/com/rakib/securitywithjwt/entity/User.java
+++ src/main/java/com/rakib/securitywithjwt/entity/User.java
@@ -2,7 +2,8 @@ package com.rakib.securitywithjwt.entity;
 
 import lombok.Data;
 
-import javax.persistence.Entity;
+import javax.persistence.*;
+import java.util.List;
 
 @Data
 @Entity
@@ -10,5 +11,8 @@ public class User extends BaseEntity {
     private String userName;
     private String email;
     private String password;
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private List<Role> roles;
 
 }
