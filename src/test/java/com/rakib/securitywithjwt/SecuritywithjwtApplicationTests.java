4e143e7aa8cd82bf372abae703001483b17d94f8
rakibccj <rakibccj@gmail.com>
2021-10-24 20:56:06 +0600

    project initiate
    Initiate with SpringWeb, mySql Driver, Lombok

---
+++ src/test/java/com/rakib/securitywithjwt/SecuritywithjwtApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rakib.securitywithjwt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuritywithjwtApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
