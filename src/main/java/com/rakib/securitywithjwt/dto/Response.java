2a7a9ef0e001eba254a88d0a53713f4f05d6078e
rakibccj <rakibccj@gmail.com>
2021-10-25 21:10:01 +0600

    Custom Response

---
+++ src/main/java/com/rakib/securitywithjwt/dto/Response.java
@@ -0,0 +1,30 @@
+package com.rakib.securitywithjwt.dto;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.Builder;
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+@Builder
+public class Response {
+
+    @JsonInclude
+    private Long timeStamp;
+    @JsonInclude
+    private int statusCode;
+    @JsonInclude
+    private String status;
+    @JsonInclude
+    private String message;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Object content;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private int numberOfElement;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long rowCount;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private List<ErrorResponse> errors;
+}
+
