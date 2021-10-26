cffe93cce558298cadcb02ceb9c0d64cf253c904
rakibccj <rakibccj@gmail.com>
2021-10-26 07:50:37 +0600

    cros origin handle

--- src/main/java/com/rakib/securitywithjwt/annotations/ApiController.java
+++ src/main/java/com/rakib/securitywithjwt/annotations/ApiController.java
@@ -2,15 +2,20 @@ package com.rakib.securitywithjwt.annotations;
 
 import org.springframework.core.annotation.AliasFor;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.lang.annotation.*;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
 
 @Target({ElementType.TYPE})
 @Retention(RetentionPolicy.RUNTIME)
 @Documented
 @Controller
 @ResponseBody
+@CrossOrigin(origins = "*", methods =
+        {GET, POST, PUT, DELETE, OPTIONS})
 public @interface ApiController {
     @AliasFor(
             annotation = Controller.class
